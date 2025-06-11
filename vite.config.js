120a6f7d2003375252e8ebaf10af585b8cfcb051
HelloPyunny <pyunhs0121@gmail.com>
2025-06-12 00:03:04 +0900

    updated base

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/hellopyunny.github.io/',
   build: {
+    outDir: 'build',
     rollupOptions: {
       output: {
         manualChunks: undefined
